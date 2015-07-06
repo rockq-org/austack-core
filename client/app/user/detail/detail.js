7d7cb7bacdfc27a93cd79eba1d97ace7324fca8d
nihgwu <nihgwu@live.com>
2015-07-06 15:42:05 +0800

    #2 move user folder out of admin folder

---
+++ client/app/user/detail/detail.js
@@ -0,0 +1,76 @@
+/**
+ * @ngdoc overview
+ * @name austackApp.admin.user.list.detail
+ * @description
+ * The `austackApp.admin.user.list.detail` module which provides:
+ *
+ * - {@link austackApp.admin.user.list.detail.controller:UserDetailController UserDetailController}
+ *
+ * @requires ui.router
+ * @requires angularMoment
+ * @requires austackApp.auth.user
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('austackApp.user.detail', [
+      'ui.router',
+      'angularMoment',
+      'austackApp.auth.user'
+    ])
+    .config(configureUserListDetail);
+
+  /**
+   * Route configuration function configuring the passed $stateProvider.
+   * Register the 'user.detail' state with the detail template
+   * paired with the UserDetailController as 'detail' for the
+   * 'sidenav' sub view.
+   * 'user' is resolved as the user with the id found in
+   * the state parameters.
+   *
+   * @param {$stateProvider} $stateProvider - The state provider to configure
+   */
+
+  configureUserListDetail.$inject = ['$stateProvider'];
+
+  function configureUserListDetail($stateProvider) {
+    // The detail state configuration
+    var detailState = {
+      name: 'user.detail',
+      parent: 'user',
+      url: '/:id',
+      authenticate: true,
+      role: 'admin',
+      views: {
+        '': {
+          templateUrl: 'app/user/detail/detail.html',
+          controller: 'UserDetailController',
+          controllerAs: 'detail',
+          resolve: {
+            user: resolveUserFromArray
+          }
+        }
+      }
+    };
+
+    $stateProvider.state(detailState);
+  }
+
+  // inject resolveUserFromArray dependencies
+  resolveUserFromArray.$inject = ['users', '$stateParams', '_'];
+
+  /**
+   * Resolve dependencies for the user.detail state
+   *
+   * @params {Array} users - The array of users
+   * @params {$stateParams} $stateParams - The $stateParams to read the user id from
+   * @returns {Object|null} The user whose value of the _id property equals $stateParams._id
+   */
+  function resolveUserFromArray(users, $stateParams, _) {
+    return _.find(users, {
+      '_id': $stateParams.id
+    });
+  }
+
+})();
---
+++ client/app/user/list/list.js
@@ -0,0 +1,83 @@
+(function () {
+  'use strict';
+
+  /**
+   * Introduce the austackApp.admin.user.list module
+   * and configure it.
+   *
+   * @requires ui.router
+   * @requires ngMaterial
+   * @requires {austackApp.socket}
+   * @requires austackApp.mainMenu
+   * @requires components/toggleComponent
+   * @requires austackApp.admin.user.list.detail
+   * @requires austackApp.admin.user.list.edit
+   * @requires austackApp.admin.user.list.items
+   */
+
+  angular
+    .module('austackApp.user.list', [
+      'ngMaterial',
+      'ui.router',
+      'austackApp.socket',
+      'austackApp.mainMenu'
+    ])
+    .config(configUserListRoutes);
+
+  // inject configUserListRoutes dependencies
+  configUserListRoutes.$inject = ['$stateProvider', 'mainMenuProvider'];
+
+  /**
+   * Route configuration function configuring the passed $stateProvider.
+   * Register the user.list state with the list template fpr the
+   * 'main' view paired with the UserListController as 'list'.
+   *
+   * @param {$stateProvider} $stateProvider - The state provider to configure
+   */
+  function configUserListRoutes($stateProvider, mainMenuProvider) {
+    // The list state configuration
+    var listState = {
+      name: 'user.list',
+      parent: 'user',
+      url: '',
+      authenticate: true,
+      role: 'admin',
+      resolve: {
+        users: resolveUsers
+      },
+      views: {
+        '': {
+          templateUrl: 'app/user/list/list.html',
+          controller: 'UserListController',
+          controllerAs: 'list'
+        }
+      },
+      ncyBreadcrumb: {
+        label: '用户'
+      }
+    };
+
+    $stateProvider.state(listState);
+
+    mainMenuProvider.addMenuItem({
+      name: '用户',
+      state: listState.name,
+      icon: 'action:ic_perm_identity_24px',
+      order: 3
+    });
+  }
+
+  // inject resolveUsers dependencies
+  resolveUsers.$inject = ['User'];
+
+  /**
+   * Resolve dependencies for the admin.user.list state
+   *
+   * @params {User} User - The service to query users
+   * @returns {Promise} A promise that, when fullfilled, returns an array of users
+   */
+  function resolveUsers(User) {
+    return User.query().$promise;
+  }
+
+})();
