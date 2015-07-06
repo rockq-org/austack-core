8893bd74fe96381342bb8e210ee9981042ecf870
nihgwu <nihgwu@live.com>
2015-07-07 01:24:41 +0800

    #7 add stats and glanceyear

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -27,6 +27,48 @@
 
   function DashboardController() {
     var vm = this;
+
+    var massive = [{
+      date: '2015-8-3',
+      value: '1'
+    }, {
+      date: '2015-8-4',
+      value: '2'
+    }, {
+      date: '2015-9-3',
+      value: '3'
+    }, {
+      date: '2015-10-14',
+      value: '2'
+    }, {
+      date: '2015-10-13',
+      value: '8'
+    }, {
+      date: '2015-7-3',
+      value: '1'
+    }, {
+      date: '2015-7-4',
+      value: '2'
+    }, {
+      date: '2015-7-7',
+      value: '3'
+    }, {
+      date: '2015-7-14',
+      value: '2'
+    }, {
+      date: '2015-6-3',
+      value: '1'
+    }, {
+      date: '2015-6-4',
+      value: '2'
+    }, {
+      date: '2015-6-5',
+      value: '3'
+    }, {
+      date: '2015-6-14',
+      value: '2'
+    }];
+    $('#js-glanceyear').glanceyear(massive);
   }
 
 })();
