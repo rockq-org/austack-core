f6b76cbd81fde17c920d91b171f54bdd3e2b8933
Samurais <hailiang.hl.wang@gmail.com>
2015-07-12 14:39:33 +0800

    #91 enable POST api/repos/:repoName

--- server/api/repo/repo.controller.js
+++ server/api/repo/repo.controller.js
@@ -7,9 +7,12 @@
 module.exports = RepoController;
 
 var _ = require('lodash');
+var shortid = require('shortid');
+var EventProxy = require('eventproxy');
 var roles = require('../../lib/auth/roles');
 var User = require('../user/user.model').model;
 var Shape = require('../shape/shape.model');
+var Repo = require('./repo.proxy');
 
 /**
  * RepoController constructor
@@ -81,7 +84,7 @@ function _index(req, res) {
  * @return {[type]}     [description]
  */
 function _get(req, res) {
-  res.send('ok');
+  res.send('oks');
 }
 
 /**
@@ -111,5 +114,97 @@ function _put(req, res) {
  * @return {[type]}     [description]
  */
 function _post(req, res) {
-  res.send('ok');
+  var repoName = req.params['repoName'];
+  var repoBody = req.body;
+
+  var ep = new EventProxy();
+
+  ep.all('doc', function (doc) {
+    logger.debug('%s new doc created. %j', repoName, doc);
+    res.json({
+      rc: 1,
+      data: doc
+    });
+  });
+
+  ep.fail(function (err) {
+    logger.error(err);
+    res.json({
+      rc: 0,
+      error: err
+    })
+  });
+
+  if (!repoName) {
+    return ep.throw(new Error('Repo name is required.'));
+  }
+
+  Shape.findOne({
+      name: repoName
+    })
+    .then(function (doc) {
+      if (!doc) {
+        return new Error('Requested repo not exist.');
+      } else {
+        return doc;
+      }
+    }, function (err) {
+      ep.throw(err);
+    })
+    .then(function (doc) {
+      // check permission
+      if (_hasPermission(req, doc)) {
+        var M = Repo.getModel(doc);
+        var m = new M();
+        var keys = _.keys(repoBody);
+        _.each(keys, function (key) {
+          m[key] = repoBody[key];
+        });
+        m.uid = shortid.generate();
+        m.save(function (err, result) {
+          if (err) {
+            ep.throw(err);
+          } else {
+            var resultJSON = result.toJSON();
+            delete resultJSON.__v;
+            delete resultJSON._id;
+            ep.emit('doc', resultJSON);
+          }
+        });
+      } else {
+        ep.throw(new Error('Permission deny.'));
+      }
+    }, function (err) {
+      ep.throw(err);
+    });
+
+}
+
+/**
+ * check if user hasPermission
+ * @param  {[type]}  req [description]
+ * @param  {[type]}  res [description]
+ * @return {Boolean}     [description]
+ */
+function _hasPermission(req, resource) {
+  if (req.userInfo.role == 'root') {
+    return true;
+  }
+  // assign the requested resource into req before check.
+  // http://stackoverflow.com/questions/11637353/comparing-mongoose-id-and-strings
+  // _id and ownerId are object.
+  if (resource.ownerId.equals(req.userInfo._id)) {
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * get mongoose model by repo name.
+ * @param  {[type]} name [description]
+ * @return {[type]}      [description]
+ */
+function _getModelByRepoName(name) {
+
 }
