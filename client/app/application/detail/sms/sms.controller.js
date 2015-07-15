291248791812c2a9f00d0021ce784953eecf99ed
nihgwu <nihgwu@live.com>
2015-07-15 16:17:57 +0800

    #29 setup ui

--- client/app/application/detail/sms/sms.controller.js
+++ client/app/application/detail/sms/sms.controller.js
@@ -18,6 +18,12 @@
   function ApplicationSMSController($state, application) {
     var vm = this;
 
+    vm.smsTemplates = {
+      "reg_sms": "APP_NAME 验证码 %P%，请在五分内注册账号。",
+      "reset_pwd_sms": "APP_NAME 验证码 %P%, 请在五分钟内重置密码。",
+      "notify_blocked_sms": "APP_NAME 因为XXX，您的账号被冻结，详情联系 XXX"
+    };
+
     // the current application to display
     vm.application = application;
   }
