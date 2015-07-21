5845ac1ab936cd7340972dce91e206ac2a079db4
Lyman Lai <lyman.lai@yaha.me>
2015-07-21 10:06:24 +0800

    #110 also remove object key

--- server/api/sampleapp/sampleapp.proxy.js
+++ server/api/sampleapp/sampleapp.proxy.js
@@ -159,11 +159,11 @@ function _ionic(res, app) {
       path.join(__dirname, '../../public/sampleapps/ionic-client/www/lib') + '/*');
 
     // delete some keys that should not expose to backend
-    app['_id'] = '';
-    app['ownerId'] = '';
-    app['isTrashed'] = '';
-    app['__v'] = '';
-    app['clientSecret'] = '';
+    app['_id'] = undefined;
+    app['ownerId'] = undefined;
+    app['isTrashed'] = undefined;
+    app['__v'] = undefined;
+    app['clientSecret'] = undefined;
 
     app.apiBaseURL = Config.apiBaseURL;
 
