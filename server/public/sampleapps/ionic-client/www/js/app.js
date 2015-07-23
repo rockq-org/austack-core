eee913a48d484b512d50202668293c006c2f433d
Lyman Lai <lyman.lai@yaha.me>
2015-07-23 12:04:45 +0800

    #113 add austack config in to app.js

--- server/public/sampleapps/ionic-client/www/js/app.js
+++ server/public/sampleapps/ionic-client/www/js/app.js
@@ -35,61 +35,94 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+      url: '/dash',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-dash': {
+          templateUrl: 'templates/tab-dash.html',
+          controller: 'DashCtrl'
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+    .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/tab-chats.html',
+            controller: 'ChatsCtrl'
+          }
+        }
+      })
+      .state('tab.chat-detail', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
+        }
+      })
+
+    .state('tab.account', {
+      url: '/account',
+      views: {
+        'tab-account': {
+          templateUrl: 'templates/tab-account.html',
+          controller: 'AccountCtrl'
+        }
+      }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
+  })
+  // austack config
+  .config(function austackConfig($stateProvider, austackProvider, jwtInterceptorProvider, $httpProvider) {
+    $stateProvider
+      .state('login', {
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: 'LoginCtrl'
+      });
+    // Configure Austack
+    austackProvider.init({
+      domain: AUSTACK_DOMAIN,
+      clientId: AUSTACK_CLIENT_ID,
+      loginState: 'login'
+    });
+
+    jwtInterceptorProvider.tokenGetter = function (store, jwtHelper, auth) {
+      var idToken = store.get('token');
+      var refreshToken = store.get('refreshToken');
+      if (!idToken || !refreshToken) {
+        return null;
+      }
+      if (jwtHelper.isTokenExpired(idToken)) {
+        return auth.refreshIdToken(refreshToken).then(function (idToken) {
+          store.set('token', idToken);
+          return idToken;
+        });
+      } else {
+        return idToken;
       }
     }
+
+    $httpProvider.interceptors.push('jwtInterceptor');
   });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
