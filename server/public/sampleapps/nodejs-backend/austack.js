68ba52f50cd4bc191e4c70b7d2ae60f432e8d73a
Lyman Lai <lyman.lai@yaha.me>
2015-07-26 16:19:58 +0800

    #161 make dave backend austack.js use Q

--- server/public/sampleapps/nodejs-backend/austack.js
+++ server/public/sampleapps/nodejs-backend/austack.js
@@ -1,10 +1,48 @@
+var Q = require('q');
 var appJSON = require('app.json');
 var request = require('superagent');
 var apiBaseURL = appJSON.apiBaseURL;
 var clientId = appJSON.clientId;
 var clientSecret = appJSON.clientSecret;
+var austack = {
+  applicationJwt: '',
+  validateUserJwt: validateUserJwt,
+  getApplicationJwt: getApplicationJwt,
+};
+module.exports = austack;
 
-exports.validateJwt = function (token) {
+function validateUserJwt(userJwt) {
+  var d = Q.defer();
+
+  var applicationJwt = '';
+  austack.getApplicationJwt()
+    .then(function (applicationJwt) {
+      request.post(apiBaseURL + '/loginRecords/validateJwt')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .set('Authorization', 'Bearer' + applicationJwt)
+        .send({
+          userJwt: userJwt
+        })
+        .end(function (err, res) {
+          if (err) {
+            return d.reject(err);
+          }
+
+          d.resolve(userJwt);
+        });
+    });
+
+  return d.promise;
+}
+
+function getApplicationJwt() {
+  var d = Q.defer();
+  if (austack.applicationJwt) {
+    d.resolve(austack.applicationJwt);
+    return d.promise;
+  }
+
   request.post(apiBaseURL + '/auth/application')
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json')
@@ -14,8 +52,12 @@ exports.validateJwt = function (token) {
     })
     .end(function (err, res) {
       if (err) {
-        return callback(err);
+        return d.reject(err);
       }
-      callback(null, res.body);
+
+      austack.applicationJwt = res.body.token;
+      d.resolve(austack.applicationJwt);
     });
+
+  return d.promise;
 }
