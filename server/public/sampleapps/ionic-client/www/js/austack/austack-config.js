3abe30acffcdc04c09e9c627895d0b5557a0e553
Lyman Lai <lyman.lai@yaha.me>
2015-08-07 15:30:21 +0800

    #190 rename austack.js to austack-module-define.js for more clear of file function

--- server/public/sampleapps/ionic-client/www/js/austack/austack-config.js
+++ server/public/sampleapps/ionic-client/www/js/austack/austack-config.js
@@ -1,26 +1,26 @@
-(function() {
-    // austack config
-    angular.module('austack')
-        .config(function austackConfig($stateProvider, austackProvider, jwtInterceptorProvider, $httpProvider) {
-            $stateProvider
-                .state(AUSTACK.loginStateName, AUSTACK.loginStateConfig);
+(function () {
+  // austack config
+  angular.module('austack')
+    .config(function austackConfig($stateProvider, austackProvider, jwtInterceptorProvider, $httpProvider) {
+      $stateProvider
+        .state(AUSTACK.loginStateName, AUSTACK.loginStateConfig);
 
-            jwtInterceptorProvider.tokenGetter = function($window, jwtHelper, austack) {
-                var jwt = $window.localStorage.getItem(AUSTACK.tokenKey);
-                var refreshToken = $window.localStorage.getItem(AUSTACK.refreshTokenKey);
-                if (!jwt || !refreshToken) {
-                    return null;
-                }
-                if (jwtHelper.isTokenExpired(jwt)) {
-                    return austack.refreshjwt(refreshToken, function(jwt) {
-                        $window.localStorage.setItem('token', jwt);
-                        return jwt;
-                    });
-                } else {
-                    return jwt;
-                }
-            }
+      jwtInterceptorProvider.tokenGetter = function ($window, jwtHelper, austack) {
+        var jwt = $window.localStorage.getItem(AUSTACK.tokenKey);
+        var refreshToken = $window.localStorage.getItem(AUSTACK.refreshTokenKey);
+        if (!jwt || !refreshToken) {
+          return null;
+        }
+        if (jwtHelper.isTokenExpired(jwt)) {
+          return austack.refreshjwt(refreshToken, function (jwt) {
+            $window.localStorage.setItem('token', jwt);
+            return jwt;
+          });
+        } else {
+          return jwt;
+        }
+      }
 
-            $httpProvider.interceptors.push('jwtInterceptor');
-        });
+      $httpProvider.interceptors.push('jwtInterceptor');
+    });
 }());
