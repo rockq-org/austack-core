90ab87c4cd6d768e8c269da90f32abfb8bf269c1
Lyman Lai <lyman.lai@yaha.me>
2015-08-07 15:12:09 +0800

    #190 update /me path to /austack-demo/me to suit server route update

--- server/public/sampleapps/ionic-client/www/js/austack/austack-variables.js
+++ server/public/sampleapps/ionic-client/www/js/austack/austack-variables.js
@@ -1,16 +1,16 @@
 var AUSTACK = {
-    clinetId: '7e37446147bc5224fa420725',
-    loginUrl: 'http://localhost:9001/tenant/login?clientId=7e37446147bc5224fa420725',
-    callbackUrl: location.href,
-    tokenKey: 'token',
-    refreshTokenKey: 'refreshToken',
+  clinetId: '7e37446147bc5224fa420725',
+  loginUrl: 'http://localhost:9001/tenant/login?clientId=7e37446147bc5224fa420725',
+  callbackUrl: location.href,
+  tokenKey: 'token',
+  refreshTokenKey: 'refreshToken',
 
-    loginStateName: 'login',
-    loginStateConfig: {
-        url: "/login",
-        templateUrl: "templates/login.html",
-        controller: 'LoginCtrl'
-    },
+  loginStateName: 'login',
+  loginStateConfig: {
+    url: "/login",
+    templateUrl: "templates/login.html",
+    controller: 'LoginCtrl'
+  },
 
-    userInfoUrl: 'http://localhost:4000/me'
+  userInfoUrl: 'http://localhost:4000/austack-demo/me'
 };
