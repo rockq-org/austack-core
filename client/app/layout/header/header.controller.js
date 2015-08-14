04a31cf992f45758c75eccb55e4665bea9fce141
nihgwu <nihgwu@live.com>
2015-08-14 23:21:23 +0800

    #236 fixed

--- client/app/layout/header/header.controller.js
+++ client/app/layout/header/header.controller.js
@@ -39,11 +39,11 @@
     vm.closeSidebar = closeSidebar;
 
     function closeSidebar() {
-      return $mdSidenav(vm.sidenavId).close();
+      return $mdSidenav(sidenavId).close();
     }
 
     function openSidebar() {
-      return $mdSidenav(vm.sidenavId).open();
+      return $mdSidenav(sidenavId).open();
     }
 
     function profile(ev) {
